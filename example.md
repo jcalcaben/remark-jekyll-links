40fab193471139c8abe828a0da6717c927cfba29
James Calcaben <jcalcaben+git@gmail.com>
2018-06-23 18:40:45 +0000

    Add plugin for inline jekyll links

--- example.md
+++ example.md
@@ -4,7 +4,7 @@ Foo [bar] link.
 
 Example of a [jekyll-style link]
 
-Another [link]({{page.baseurl}}{% link hello/world2/index.md %})
+Another [link]({{page.baseurl}}{% link hello/world2/index.md %}) on this page.
 
 [bar]: google.com
 [jekyll-style link]: {{page.baseurl}}{% link hello/world/index.md %}
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-jekyllDefinitions = require('./lib/jekyll-definitions.js');
+jekyllDefinitions = require('./lib/jekyll-definitions');
+jekyllLinks = require('./lib/jekyll-links')
 unified = require('unified');
 markdown = require('remark-parse')
 stringify = require('remark-stringify')
@@ -10,13 +11,14 @@ let input = reader.sync('example.md','utf8')
 let result = unified()
               .use(markdown)
               .use(stringify)
+              .use(jekyllLinks)
               .use(jekyllDefinitions)
               
-//let nodes = result.parse(input)
-//
-//visit(nodes,'root',(node)=>{
-//  console.log(node);
-//})
+let nodes = result.parse(input)
+
+visit(nodes,'paragraph',(node)=>{
+  console.log(node);
+})
 
 let output = result.processSync(input);
 
